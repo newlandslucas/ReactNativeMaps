000518dd9c962f5953235c11371b426571ae913d
Lucas Newlands <lucasnewlands@MacBook-Pro-de-Lucas.local>
2023-03-27 15:14:16 -0300

    Project finished

---
+++ styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1c1c1c',
+    },
+    mapView: {
+        flex: 1,
+        width: '100%'
+    }
+})
